174ddb869ef56a8e8204f9862f144f906ee6d594
Vlad Dukhnich <vdukhnich@itquick.ru>
2019-12-23 22:50:44 +0300

    Updates

---
+++ precache-manifest.a958a7bd546c02d64d62726261e865d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86cbcd0769f837e78e90dc5092f48a22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3331b691e168e40f527f",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "eff1a1a2253d4b618b13",
+    "url": "/static/js/2.ef4e2cd7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.ef4e2cd7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3331b691e168e40f527f",
+    "url": "/static/js/main.60245e28.chunk.js"
+  },
+  {
+    "revision": "94908ca1d5bb7cbac37e",
+    "url": "/static/js/runtime-main.e0a25877.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
